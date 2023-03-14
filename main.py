c6c682eb37a298a0c89fa43046eb90466e887343
mulchus <mulch@list.ru>
2023-03-14 23:19:41 +0300

    Made path to service_account.json in project_dir

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import gspread
+from pathlib import Path
 
-gc = gspread.service_account()
 
-sh = gc.open('smm-planer-table')
+def main():
+    filename = str(Path.joinpath(Path.cwd(), 'service_account.json'))
+    gc = gspread.service_account(filename=filename)
+    sh = gc.open('smm-planer-table')
 
-print(sh.sheet1.get('A1'))
+    print(sh.sheet1.get('A1'))
+
+
+if __name__ == '__main__':
+    main()
