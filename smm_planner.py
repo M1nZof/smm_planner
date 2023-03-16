23816c7b02779666cdc3035f606e3742e95f35c2
M1nZof <maxykl38@gmail.com>
2023-03-16 21:23:26 +0300

    Added social network validation

--- smm_planner.py
+++ smm_planner.py
@@ -1,3 +1,4 @@
+import time
 from urllib import parse
 
 import gspread
@@ -9,6 +10,9 @@ from pathlib import Path
 from environs import Env
 
 from social_networks_handlers import vk_publication, ok_publication
+from social_networks_handlers.ok_publication import publication_post_ok
+from social_networks_handlers.tg_publication import send_telegram_post
+from social_networks_handlers.vk_publication import publication_post_vk
 
 GOOGLE_CREDENTIALS = gspread.service_account(Path.joinpath(Path.cwd(), 'service_account.json').__str__())
 SPREADSHEET = GOOGLE_CREDENTIALS.open('smm-planer-table')
@@ -19,40 +23,46 @@ def main():
     env = Env()
     env.read_env()
 
-    # telegram_bot_token = env('TELEGRAM_BOT_TOKEN')
-    # telegram_chat_id = env('TELEGRAM_CHAT_ID')
+    telegram_bot_token = env('TELEGRAM_BOT_TOKEN')
+    telegram_chat_id = env('TELEGRAM_CHAT_ID')
+
+    while True:
+        all_posts = sheet_functions.get_all_records()
+
+        for post_number, post in enumerate(all_posts, start=2):
+            formatted_datetime = sheet_functions.get_formatted_datetime(post)
+            datetime_now = sheet_functions.get_datetime_now()
+
+            if formatted_datetime <= datetime_now and post['public_fact'] != '': # TODO убрать проверку факта публикации
+                post_text, image_file_name = get_posts_text_imagefile(post_number)
+                if post['social_network'] == 'Telegram':
+                    send_telegram_post(telegram_bot_token, telegram_chat_id, post)
+                elif post['social_network'] == 'VK':
+                    publication_post_vk(post_text, image_file_name)
+                elif post['social_network'] == 'OK':
+                    publication_post_ok(post_text, image_file_name)
+                sheet_functions.post_cell_text(post_number, 7, datetime_now)
+                time.sleep(300)
+
+    # # всего постов
+    # posts_count = sheet_functions.get_posts_count()
     #
-    # while True:
-    #     all_posts = sheet_functions.get_all_records()
+    # # задаем номер публикуемого поста
+    # post_number = 5
     #
-    #     for post_number, post in enumerate(all_posts, start=2):
-    #         formatted_datetime = sheet_functions.get_formatted_datetime(post)
-    #         datetime_now = sheet_functions.get_datetime_now()
+    # # проверяем на верность
+    # if post_number > posts_count:
+    #     print(f'{post_number} - неверный номер поста')
+    #     exit()
     #
-    #         if formatted_datetime <= datetime_now and post['public_fact'] != '':
-    #             send_telegram_post(telegram_bot_token, telegram_chat_id, post)
-    #             sheet_functions.post_cell_text(post_number, 7, datetime_now)
-    #             time.sleep(300)
-
-    # всего постов
-    posts_count = sheet_functions.get_posts_count()
-
-    # задаем номер публикуемого поста
-    post_number = 5
-
-    # проверяем на верность
-    if post_number > posts_count:
-        print(f'{post_number} - неверный номер поста')
-        exit()
-
-    # получаем текст поста и имя файла картинки
-    post_text, image_file_name = get_posts_text_imagefile(post_number)
-
-    # публикуем пост Вконтаке
-    vk_publication.publication_post_vk(post_text, image_file_name)
-
-    # публикуем пост в Одноклассниках
-    ok_publication.publication_post_ok(post_text, image_file_name)
+    # # получаем текст поста и имя файла картинки
+    # post_text, image_file_name = get_posts_text_imagefile(post_number)
+    #
+    # # публикуем пост Вконтаке
+    # vk_publication.publication_post_vk(post_text, image_file_name)
+    #
+    # # публикуем пост в Одноклассниках
+    # ok_publication.publication_post_ok(post_text, image_file_name)
 
 
 def get_posts_text_imagefile(post_number):
