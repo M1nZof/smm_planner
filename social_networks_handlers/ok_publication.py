343a48f72b6daf5be23cef2719db589c8b40ab35
SGK <55636018+SGKespace@users.noreply.github.com>
2023-03-17 01:08:48 +0400

    если нет картинки

--- social_networks_handlers/ok_publication.py
+++ social_networks_handlers/ok_publication.py
@@ -1,6 +1,19 @@
 import os
 from ok_api import OkApi, Upload
 from dotenv import load_dotenv
+from PIL import Image, ImageDraw, ImageFont
+import sys
+
+def not_img(post_text):
+    img = Image.new('RGBA', (200, 200), 'white')
+    idraw = ImageDraw.Draw(img)
+    img.save('post.png')
+    image = Image.open('post.png')
+    idraw = ImageDraw.Draw(image)
+
+    font = ImageFont.truetype('FreeMono.ttf', size=18)
+    idraw.text((10, 10), post_text, font=font, color='red')
+    image.save('post.png')
 
 
 def publication_post_ok(post_text, image_file_name):
@@ -9,6 +22,10 @@ def publication_post_ok(post_text, image_file_name):
     application_key = os.environ["OK_APPLICATION_KEY"]
     application_secret_key = os.environ["OK_SECRET_KEY"]
     album = os.environ["OK_ALBUM"]
+
+    if not image_file_name:
+        not_img(post_text)
+        image_file_name = 'post.png'
 
     ok = OkApi(access_token=access_token,
                application_key=application_key,
@@ -20,20 +37,17 @@ def publication_post_ok(post_text, image_file_name):
     for photo_id in upload_response['photos']:
         token = upload_response['photos'][photo_id]['token']
         response = ok.photosV2.commit(photo_id=photo_id, token=token, comment=post_text)
-        print(response.json())
-
-    print(ok.friends.get())
-
-    response = ok.friends.get(sort_type='PRESENT')
-    print(response.json())
+    try:
+        if response.json()['photos'][0]['status'] == 'SUCCESS':
+            return True
+    except:
+        return False
 
 
 def main():
     post_text = 'Какая то бабуйня в космосе'
-    image_file_name = '2.jpg'
-
-    publication_post_ok(post_text, image_file_name)
-
+    image_file_name = ''
+    luck = publication_post_ok(post_text, image_file_name)
 
 
 if __name__ == '__main__':
