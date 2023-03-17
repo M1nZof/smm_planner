1b7787ab42cbcf75dad565667071d5e3803bbbc2
SGK <55636018+SGKespace@users.noreply.github.com>
2023-03-17 10:43:29 +0400

    Добавил функцию удаления картинки по ID

--- social_networks_handlers/ok_publication.py
+++ social_networks_handlers/ok_publication.py
@@ -2,6 +2,7 @@ import os
 from ok_api import OkApi, Upload
 from dotenv import load_dotenv
 from PIL import Image, ImageDraw, ImageFont
+import requests
 
 
 def not_img(text):
@@ -14,6 +15,35 @@ def not_img(text):
     font = ImageFont.truetype('FreeMono.ttf', size=18)
     idraw.text((10, 30), text, fill=black, font=font, color='red')
     image.save('post.png')
+
+
+def delet_img(photo_id):
+    load_dotenv()
+    access_token = os.environ["OK_ACCESS_TOKEN"]
+    application_key = os.environ["OK_APPLICATION_KEY"]
+    application_secret_key = os.environ["OK_SECRET_KEY"]
+    ok_user = os.environ["OK_USER"]
+
+    headers = {'client_id': ok_user}
+    params = {
+        'scope': 512001823570,
+        'access_token': access_token,
+        'application_key': application_key,
+        'session_secret_key': application_secret_key,
+        'response_type': 'token',
+        'redirect_uri': 'https://apiok.ru/oauth_callback',
+        'photo_id': photo_id,
+        'method': 'photos.deletePhoto'
+    }
+    url_sesion = 'https://api.ok.ru/fb.do'
+    response = requests.post(url_sesion, headers=headers, params=params)
+    try:
+        response.raise_for_status()
+        response_message = response.json()
+        return True
+    except:
+        print('Что то опять не так')
+        return False
 
 
 def publication_post_ok(post_text, image_file_name):
@@ -46,9 +76,11 @@ def publication_post_ok(post_text, image_file_name):
 
 
 def main():
-    post_text = 'Какая то бабуйня в космосе'
+    post_text = 'Это сообщение было без картинки'
     image_file_name = ''
-    luck = publication_post_ok(post_text, image_file_name)
+    photo_id = publication_post_ok(post_text, image_file_name)
+    print(photo_id)
+    delet_img(photo_id)
 
 
 if __name__ == '__main__':
