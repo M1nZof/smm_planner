5a0328dfb429300345657d7b98fca63b33f092dc
M1nZof <maxykl38@gmail.com>
2023-03-20 14:24:10 +0300

    Handling OK errors, which one were in dict, instead of in ok_exceptions.OkApiException

--- social_networks_handlers/ok_publication.py
+++ social_networks_handlers/ok_publication.py
@@ -50,18 +50,28 @@ def publication_post_ok(post_text, image_file_name):
                application_key=application_key,
                application_secret_key=application_secret_key)
     upload = Upload(ok)
+
     try:
         upload_response = upload.photo(photos=[image_file_name], album=album)
         for photo_id in upload_response['photos']:
             token = upload_response['photos'][photo_id]['token']
             response = ok.photosV2.commit(photo_id=photo_id, token=token, comment=post_text)
-            photo_id = response.json()['photos'][0]['photo_id']
-            return photo_id
+
+            try:
+                photo_id = response.json()['photos'][0]['photo_id']
+            except KeyError:
+                raise SocialNetworkError({'col': 7, 'message': response.json()['error_msg']})
+
+        if image_file_name == 'post.png':
+            Path(Path.cwd(), image_file_name).unlink()
+
+        return photo_id
+
     except ok_exceptions.OkApiException as error:
         raise SocialNetworkError({'col': 7, 'message': error.__dict__['message']['error_msg']})
+
     except requests.exceptions.HTTPError:
         raise SocialNetworkError({'col': 7, 'message': 'HTTPError'})
+
     except requests.exceptions.ConnectionError:
         raise requests.exceptions.ConnectionError
-    except KeyError:
-        raise SocialNetworkError({'col': 7, 'message': response.json()['error_msg']})
