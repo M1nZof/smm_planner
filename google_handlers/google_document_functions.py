ca22b5612606b2c7546c3053441448554d1db607
M1nZof <maxykl38@gmail.com>
2023-03-20 03:32:39 +0300

    Code optimization:
    - 2 cycles instead of 3 throw scripts;
    - Better naming;
    - Handling case, when post without image

--- google_handlers/google_document_functions.py
+++ google_handlers/google_document_functions.py
@@ -1,58 +1,44 @@
 import json
-from urllib.parse import urlparse
 
 import gdown
 from bs4 import BeautifulSoup
 
 
-def get_post_text(link_google_document):
+def get_google_document_text_and_image_url(link_google_document):
     document_name = 'temp_post_file'
     gdown.download(url=link_google_document, output=document_name, fuzzy=True, quiet=True)
-    text = get_google_document_text(document_name)
-    return text
+    text_with_photo = get_doc_text_and_image_url(document_name)
+    return text_with_photo
 
 
-def get_post_image_url(link_google_document):
-    document_name = 'temp_post_file'
-    gdown.download(url=link_google_document, output=document_name, fuzzy=True, quiet=True)
-    image_url = get_google_document_image_url(document_name)
-    return image_url
-
-
-def get_google_document_text(document_name):
-    with open(document_name, 'r', encoding='UTF-8') as file:
+def get_doc_text_and_image_url(document):
+    with open(document, 'r', encoding='UTF-8') as file:
         soup = BeautifulSoup(file, 'html.parser')
         scripts = soup.find_all({'script': 'nonce'})
-        text = ''
         for script in scripts:
             if script.text.startswith('DOCS_modelChunk ='):
-                text = script.text.replace('DOCS_modelChunk = ', '')
-                excess_text_part_char = text.find('; DOCS_modelChunkLoadStart')
-                text = text[:excess_text_part_char]
-                post_text = json.loads(text)[0]['s']
-                break
-    return post_text
+                docs_model_chunk = script.text.replace('DOCS_modelChunk = ', '')
+                excess_chunk_part_char = docs_model_chunk.find('; DOCS_modelChunkLoadStart')
+                doc_list = json.loads(docs_model_chunk[:excess_chunk_part_char])
+
+                image_url = get_google_document_image_url(doc_list, scripts)
+                post_text = doc_list[0]['s']
+
+                return post_text.replace('*', ''), image_url
 
 
-def get_google_document_image_url(document_name):
-    with open(document_name, 'r', encoding='UTF-8') as file:
-        soup = BeautifulSoup(file, 'html.parser')
-        scripts = soup.find_all({'script': 'nonce'})
-        text = ''
-        for script in scripts:
-            if script.text.startswith('DOCS_modelChunk ='):
-                text = script.text.replace('DOCS_modelChunk = ', '')
-                excess_text_part_char = text.find('; DOCS_modelChunkLoadStart')
-                text = text[:excess_text_part_char]
-                text2 = json.loads(text)[2]['epm']['ee_eo']['i_cid']
-                break
-        for script in scripts:
-            if script.text.find(text2) > 0:
-                start_text_part_char = script.text.find(text2)
-                text3 = script.text[start_text_part_char:]
-                start_text_part_char = text3.find('":"')
-                text3 = text3[start_text_part_char:]
-                excess_text_part_char = text3.find('"}')
-                image_url = text3[3:excess_text_part_char]
-                break
-    return image_url
+def get_google_document_image_url(doc_list, scripts):
+    try:
+        script_doc_id = doc_list[2]['epm']['ee_eo']['i_cid']
+    except KeyError:
+        return
+    for script in scripts:
+        if script.text.find(script_doc_id) > 0:
+            start_script_doc_id_char = script.text.find(script_doc_id)
+            start_script_doc_id = script.text[start_script_doc_id_char:]
+            start_image_url_char = start_script_doc_id.find('https')
+            start_image_url = start_script_doc_id[start_image_url_char:]
+            excess_image_url_char = start_image_url.find('"}')
+            image_url = start_image_url[:excess_image_url_char]
+            return image_url
+
